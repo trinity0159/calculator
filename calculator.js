1e93272d137b89332c351b68002205c6be976822
Katrina Meyer <katrina@Katrinas-MacBook-Pro.local>
2017-06-08 18:36:55 -0400

    first commit

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function addNumbers(numberOne, numberTwo){
+	return numberOne + numberTwo;
+}
+
+function subtractNumbers(numberOne, numberTwo){
+	return numberOne - numberTwo;
+}
+
+function multiplyNumbers(numberOne, numberTwo){
+	return numberOne * numberTwo;
+}
+
+function divideNumbers(numberOne, numberTwo){
+	return numberOne / numberTwo;
+}
